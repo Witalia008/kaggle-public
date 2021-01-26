06ef38b653f810f16baf3ce307d8c76ab0dbc9bd
Vitalii Kozhukhivskyi <kojvitalij@gmail.com>
2021-01-26 17:05:10 +0000

    Add a base cassava CNN model with inference.

---
+++ cassava-leaf-disease-classification/cassava-inference.py
@@ -0,0 +1,109 @@
+import os
+import shutil
+
+import pandas as pd
+import numpy as np
+import tensorflow as tf
+from tensorflow.keras.models import load_model
+from tensorflow.data.experimental import AUTOTUNE
+from keras_preprocessing.image import ImageDataGenerator
+
+DEVMODE = os.getenv("KAGGLE_MODE") == "DEV"
+print(f"DEV MODE: {DEVMODE}")
+
+INPUT_FOLDER = "/kaggle/input/cassava-leaf-disease-classification/"
+WORK_DIR = "/kaggle/working"
+
+IMAGE_SIZE = (224, 224)
+
+
+def _parse_tfrecord(example_proto):
+    feature_descriptions = {
+        "image_name": tf.io.FixedLenFeature([], tf.string, default_value=""),
+        "image": tf.io.FixedLenFeature([], tf.string, default_value=""),
+    }
+    example = tf.io.parse_single_example(example_proto, feature_descriptions)
+
+    image = tf.io.decode_jpeg(example["image"], channels=3)
+    image = tf.image.resize(image, IMAGE_SIZE)
+    image = tf.cast(image, tf.float32) / 255.0
+
+    return image, example["image_name"]
+
+
+def get_test_dataset():
+    test_files = tf.data.Dataset.list_files(os.path.join(INPUT_FOLDER, "test_tfrecords/*.tfrec"))
+
+    dataset = tf.data.TFRecordDataset(test_files, num_parallel_reads=AUTOTUNE)
+
+    dataset = dataset.map(_parse_tfrecord, num_parallel_calls=AUTOTUNE)
+    dataset = dataset.batch(16).prefetch(AUTOTUNE)
+
+    image_names = [bytes.decode(example[-1].numpy()[0]) for example in dataset]
+
+    return dataset, image_names
+
+
+def get_test_generator():
+    test_datagen = ImageDataGenerator(rescale=1 / 255.0)
+
+    # Copy into a temp folder so that test_images would have a subfolder to make flow_from_directory happy.
+    test_dir = os.path.join(INPUT_FOLDER, "train_images")
+    new_test_dir = os.path.join(WORK_DIR, "cassava-test")
+    if not os.path.exists(new_test_dir):
+        os.makedirs(new_test_dir, exist_ok=True)
+        shutil.copytree(test_dir, os.path.join(new_test_dir, "test_images"))
+
+    test_dataset = test_datagen.flow_from_directory(
+        directory=new_test_dir,
+        target_size=IMAGE_SIZE,
+        class_mode=None,  # No labels, just images.
+        shuffle=False,  # To preserve the order for prediction.
+        batch_size=1,  # To sample all the images exactly once.
+    )
+
+    image_names = [os.path.basename(image_name) for image_name in test_dataset.filenames]
+
+    return test_dataset, image_names
+
+
+def store_predictions(predictions, image_names):
+    predictions = np.argmax(predictions, axis=-1)
+
+    submission_file = os.path.join(WORK_DIR, "submission.csv")
+    pd.DataFrame({"image_id": image_names, "label": predictions}).to_csv(submission_file, index=False)
+    print(pd.read_csv(submission_file).head())
+
+
+def run_predictions(model):
+    predictions = []
+    test_image_names = []
+
+    test_dir = os.path.join(INPUT_FOLDER, "test_images")
+
+    for image_name in os.listdir(test_dir):
+        image = tf.keras.preprocessing.image.load_img(os.path.join(test_dir, image_name), target_size=IMAGE_SIZE)
+        image = tf.keras.preprocessing.image.img_to_array(image)
+        image = np.expand_dims(image, axis=0)
+        image = image / 255.0
+
+        cur_prediction = model.predict(image)
+
+        predictions.append(cur_prediction[0])
+        test_image_names.append(image_name)
+
+    return predictions, test_image_names
+
+
+if __name__ == "__main__":
+    model_location = WORK_DIR if DEVMODE else "/kaggle/input/cassava-clean"
+    model = load_model(os.path.join(model_location, "cassava.h5"))
+
+    # test_dataset, test_image_names = get_test_dataset()
+    # test_dataset, test_image_names = get_test_generator()
+
+    # test_dataset.reset()
+    # predictions = model.predict(test_dataset)
+
+    predictions, test_image_names = run_predictions(model)
+    store_predictions(predictions, test_image_names)
---
+++ cassava-leaf-disease-classification/cassava.py
@@ -0,0 +1,184 @@
+# import glob
+import json
+import os
+import random
+
+import numpy as np
+import pandas as pd
+import tensorflow as tf
+from keras_preprocessing.image import ImageDataGenerator
+from tensorflow.data.experimental import AUTOTUNE
+from tensorflow.keras.layers import Conv2D, Dense, Dropout, Flatten, Input, MaxPooling2D
+from tensorflow.keras.models import Sequential
+
+print(tf.__version__)
+
+DEVMODE = os.getenv("KAGGLE_MODE") == "DEV"
+print(f"DEV MODE: {DEVMODE}")
+
+INPUT_FOLDER = "/kaggle/input/cassava-leaf-disease-classification/"
+WORK_DIR = "/kaggle/working"
+
+SEED = hash("cassava-leaf-disease-classification") % 100
+
+DATASET_SIZE = 21367
+VALID_DATASET_RATIO = 0.2
+BATCH_SIZE = 16
+IMAGE_SIZE = (224, 224)
+
+EPOCHS = 1 if DEVMODE else 10
+STEPS = 30 if DEVMODE else None  # int(DATASET_SIZE * (1 - VALID_DATASET_RATIO) / BATCH_SIZE * 1.5)
+VALID_STEPS = 10 if DEVMODE else None  # None - use all data for validation
+
+
+def seed_everything(seed=SEED):
+    random.seed(SEED)
+    np.random.seed(SEED)
+    tf.random.set_seed(SEED)
+    os.environ["PYTHONHASHSEED"] = str(SEED)
+    os.environ["TF_DETERMINISTIC_OPS"] = "1"
+
+
+def get_disease_map():
+    with open(os.path.join(INPUT_FOLDER, "label_num_to_disease_map.json")) as f:
+        disease_map: dict = json.load(f)
+
+    disease_map = {int(disease_id): disease_name for (disease_id, disease_name) in disease_map.items()}
+
+    print(disease_map)
+    return disease_map
+
+
+def _parse_tfrecord(example_proto):
+    feature_descriptions = {
+        "image_name": tf.io.FixedLenFeature([], tf.string, default_value=""),
+        "target": tf.io.FixedLenFeature([], tf.int64, default_value=-1),
+        "image": tf.io.FixedLenFeature([], tf.string, default_value=""),
+    }
+    example = tf.io.parse_single_example(example_proto, feature_descriptions)
+
+    image = tf.io.decode_jpeg(example["image"], channels=3)
+    image = tf.image.resize(image, IMAGE_SIZE)
+    image = tf.cast(image, tf.float32) / 255.0
+
+    return image, example["target"], example["image_name"]
+
+
+def get_train_val_datasets():
+    train_files = tf.data.Dataset.list_files(os.path.join(INPUT_FOLDER, "train_tfrecords/ld_train*.tfrec"))
+
+    dataset = tf.data.TFRecordDataset(train_files, num_parallel_reads=AUTOTUNE)
+    dataset = dataset.map(_parse_tfrecord, num_parallel_calls=AUTOTUNE).map(
+        lambda x, y, _: (x, y), num_parallel_calls=AUTOTUNE
+    )
+
+    dataset = dataset.shuffle(128)
+
+    valid_dataset_size = int(DATASET_SIZE * VALID_DATASET_RATIO)
+
+    valid_dataset = dataset.take(valid_dataset_size).batch(BATCH_SIZE).prefetch(AUTOTUNE)
+    train_dataset = dataset.skip(valid_dataset_size).batch(BATCH_SIZE).prefetch(AUTOTUNE)
+
+    return train_dataset, valid_dataset
+
+
+def get_train_val_generators():
+    train_data = pd.read_csv(os.path.join(INPUT_FOLDER, "train.csv"))
+    train_data.label = train_data.label.astype("str")
+    print(train_data.head())
+
+    train_datagen = ImageDataGenerator(
+        rescale=1 / 255.0,
+        validation_split=VALID_DATASET_RATIO,
+        rotation_range=45,
+        width_shift_range=0.2,
+        height_shift_range=0.2,
+        zoom_range=0.2,
+        horizontal_flip=True,
+        vertical_flip=True,
+        shear_range=0.2,
+        fill_mode="nearest",
+    )
+
+    train_dataset = train_datagen.flow_from_dataframe(
+        train_data,
+        subset="training",
+        directory=os.path.join(INPUT_FOLDER, "train_images"),
+        x_col="image_id",
+        y_col="label",
+        target_size=IMAGE_SIZE,
+        batch_size=BATCH_SIZE,
+        class_mode="sparse",
+        seed=SEED,
+    )
+
+    valid_dataset = train_datagen.flow_from_dataframe(
+        train_data,
+        subset="validation",
+        directory=os.path.join(INPUT_FOLDER, "train_images"),
+        x_col="image_id",
+        y_col="label",
+        target_size=IMAGE_SIZE,
+        batch_size=BATCH_SIZE,
+        class_mode="sparse",
+        shuffle=False,
+        seed=SEED,
+    )
+
+    return train_dataset, valid_dataset
+
+
+def build_model():
+    model = Sequential(
+        [
+            Input(shape=IMAGE_SIZE + (3,)),
+            Conv2D(filters=32, kernel_size=5, activation="relu"),
+            MaxPooling2D((3, 3)),
+            Conv2D(64, 5, activation="relu"),
+            MaxPooling2D((3, 3)),
+            Conv2D(128, 5, activation="relu"),
+            MaxPooling2D((3, 3)),
+            Flatten(),
+            Dense(1024, activation="relu"),
+            Dropout(0.2),
+            Dense(5, activation="softmax"),
+        ]
+    )
+
+    optimizer = tf.keras.optimizers.Adam(learning_rate=0.001)
+
+    metrics = [tf.keras.metrics.SparseCategoricalAccuracy(name="accuracy")]
+
+    model.compile(loss="sparse_categorical_crossentropy", optimizer=optimizer, metrics=metrics)
+
+    print(model.summary())
+
+    return model
+
+
+def train_model(model: tf.keras.models.Model, train_dataset, valid_dataset):
+    history = model.fit(
+        train_dataset,
+        epochs=EPOCHS,
+        steps_per_epoch=STEPS,
+        validation_data=valid_dataset,
+        validation_steps=VALID_STEPS,
+        shuffle=False,  # Datasets are already shuffled
+    )
+
+    model.save(os.path.join(WORK_DIR, "cassava.h5"))
+
+    return history
+
+
+if __name__ == "__main__":
+    seed_everything()
+
+    disease_map = get_disease_map()
+
+    model = build_model()
+
+    # train_dataset, valid_dataset = get_train_val_datasets()
+    train_dataset, valid_dataset = get_train_val_generators()
+
+    history = train_model(model, train_dataset, valid_dataset)

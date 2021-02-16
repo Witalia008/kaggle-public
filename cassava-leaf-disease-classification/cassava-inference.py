4e2e99f223bb6a7cb3de51210312348ffcfd89cb
Vitalii Kozhukhivskyi <kojvitalij@gmail.com>
2021-02-16 15:31:40 +0000

    Add Test Time Augmentation.

--- cassava-leaf-disease-classification/cassava-inference.py
+++ cassava-leaf-disease-classification/cassava-inference.py
@@ -1,3 +1,4 @@
+import gc
 import os
 import shutil
 
@@ -6,8 +7,11 @@ import pandas as pd
 import tensorflow as tf
 from keras_preprocessing.image import ImageDataGenerator
 from tensorflow.data.experimental import AUTOTUNE
+from tensorflow.keras import Model
+from tensorflow.keras.applications.inception_v3 import preprocess_input
 from tensorflow.keras.losses import sparse_categorical_crossentropy
 from tensorflow.keras.models import load_model
+import cv2
 
 DEVMODE = os.getenv("KAGGLE_MODE") == "DEV"
 print(f"DEV MODE: {DEVMODE}")
@@ -16,6 +20,8 @@ INPUT_FOLDER = "/kaggle/input/cassava-leaf-disease-classification/"
 WORK_DIR = "/kaggle/working"
 
 IMAGE_SIZE = (512, 512)
+
+N_FOLD = 10
 
 
 def _parse_tfrecord(example_proto):
@@ -69,29 +75,51 @@ def get_test_generator():
 
 
 def store_predictions(predictions, image_names):
-    predictions = np.argmax(predictions, axis=-1)
-
     submission_file = os.path.join(WORK_DIR, "submission.csv")
     pd.DataFrame({"image_id": image_names, "label": predictions}).to_csv(submission_file, index=False)
     print(pd.read_csv(submission_file).head())
 
 
-def run_predictions(model):
+def softmax(x):
+    e_x = np.exp(x - np.max(x, axis=-1, keepdims=True))
+    return e_x / e_x.sum(axis=-1, keepdims=True)
+
+
+def run_predictions(model: Model):
     predictions = []
     test_image_names = []
 
     test_dir = os.path.join(INPUT_FOLDER, "test_images")
 
-    for image_name in os.listdir(test_dir):
+    datagen = ImageDataGenerator(
+        preprocessing_function=preprocess_input,
+        horizontal_flip=True,
+        vertical_flip=True,
+        rotation_range=45,
+        zoom_range=0.2,
+        width_shift_range=0.2,
+        height_shift_range=0.2,
+        shear_range=0.2,
+        brightness_range=(0.8, 1.2),
+        fill_mode="nearest",
+    )
+
+    for image_idx, image_name in enumerate(os.listdir(test_dir)):
         image = tf.keras.preprocessing.image.load_img(os.path.join(test_dir, image_name), target_size=IMAGE_SIZE)
         image = tf.keras.preprocessing.image.img_to_array(image)
         image = np.expand_dims(image, axis=0)
-        image = tf.keras.applications.inception_v3.preprocess_input(image)
 
-        cur_prediction = model.predict(image)
+        images = np.broadcast_to(image, (N_FOLD,) + image.shape[1:])
+        # Use predict_on_batch since looks like predict has a memory leak...
+        prediction = model.predict_on_batch(next(datagen.flow(images)))
+        prediction = softmax(prediction)
+        prediction = np.mean(prediction, axis=0, keepdims=True)
 
-        predictions.append(cur_prediction[0])
+        predictions.append(int(np.argmax(prediction, axis=-1).squeeze()))
         test_image_names.append(image_name)
+
+        if image_idx % 100 == 0:  # Only run every so many images.
+            gc.collect()
 
     return predictions, test_image_names
 
@@ -104,6 +132,7 @@ if __name__ == "__main__":
             "bi_tempered_loss": sparse_categorical_crossentropy
         },  # Loss is not used for inference, so assign to whatever.
     )
+    print(model.summary())
 
     # test_dataset, test_image_names = get_test_dataset()
     # test_dataset, test_image_names = get_test_generator()

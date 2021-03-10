1fab3b7b5fa7297a665f55251bc9b14506e26eec
Vitalii Kozhukhivskyi <kojvitalij@gmail.com>
2021-03-10 18:26:35 +0000

    Allow to have /kaggle/working locally in Colab.

--- setup_colab.py
+++ setup_colab.py
@@ -11,11 +11,12 @@ def is_running_in_colab(check_env=True):
     return running_in_colab
 
 
-def setup_colab_directories_for_kaggle(check_env=True):
+def setup_colab_directories_for_kaggle(check_env=True, local_working=False):
     if not is_running_in_colab(check_env):
         return False
 
-    target_content_dirs = ["input", "working"]
+    # Only add "working" directory if it was requested to be mapped in Drive, not in local env.
+    target_content_dirs = ["input", "output"] + ([] if local_working else ["working"])
 
     drive_content_dir = Path("/content/drive/MyDrive/kaggle")
     # Make sure directories are present in Drive
@@ -31,9 +32,13 @@ def setup_colab_directories_for_kaggle(check_env=True):
 
     for content_dir in target_content_dirs:
         (kaggle_dir / content_dir).symlink_to(drive_content_dir / content_dir)
+
+    # It was requested not to map working to Drive, so create it locally.
+    if local_working:
+        (kaggle_dir / "working").mkdir()
     
     print(f"Content of Kaggle data dir ({kaggle_dir}): {list(map(str, kaggle_dir.iterdir()))}")
-    for content_dir in target_content_dirs:
+    for content_dir in target_content_dirs + (["working"] if local_working else []):
         print(f"Content of Kaggle data subdir ({kaggle_dir / content_dir}): {list(map(str, (kaggle_dir / content_dir).iterdir()))}")
     
     return True  # Is Colab
@@ -49,11 +54,11 @@ def setup_colab_drive_for_kaggle(check_env=True):
     return True  # Is Colab
 
 
-def setup_colab_for_kaggle(check_env=True):
+def setup_colab_for_kaggle(check_env=True, local_working=False):
     if not is_running_in_colab(check_env):
         return False
 
     setup_colab_drive_for_kaggle(check_env=False)
-    setup_colab_directories_for_kaggle(check_env=False)
+    setup_colab_directories_for_kaggle(check_env=False, local_working=local_working)
 
     return True  # Is Colab
